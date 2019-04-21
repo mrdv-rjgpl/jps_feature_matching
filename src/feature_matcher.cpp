08ebfbca4affc4a36ed231a5afb4943bb9557d3f
Mardava Gubbi <mgubbi1@jhu.edu>
2019-04-21 17:12:25 -0400

    Fix compile and runtime issues in SURF matcher

--- src/feature_matcher.cpp
+++ src/feature_matcher.cpp
@@ -26,20 +26,66 @@ using namespace std;
 class FeatureMatcher
 {
   private:
-    image_transport::ImageTransport *img_transport;
+    /*
+     * \brief Image subscriber object
+     */
     ros::Subscriber image_sub;
+    /*
+     * \brief Minimum hessian threshold for SURF feature extraction
+     */
     int min_hessian;
+    /*
+     * \brief Template image
+     */
     Mat img_template;
+    /*
+     * \brief Descriptor matrix for template image
+     */
     Mat desc_template;
+    /*
+     * \brief Node handler object
+     */
     ros::NodeHandle nh;
+    /*
+     * \brief Image publisher object
+     */
     ros::Publisher image_pub;
+    /*
+     * \brief Keypoint vector for template image
+     */
     vector<KeyPoint> kp_template;
+    /*
+     * \brief SURF detector object
+     */
     Ptr<SURF> surf_detector;
+    /*
+     * \brief Probability Distribution Function of validity
+     *
+     * \details The validity PDF is computed with respect to the ratio
+     * of the smallest to the second smallest distances.
+     */
     double dist_ratio_pdf[21][2];
 
   public:
+    /*
+     * \brief Constructor for FeatureMatcher class
+     *
+     * \param[in] nh The node handler object
+     * \param[in] min_hessian The minimum Hessian threshold for SURF feature
+     * extraction
+     *
+     * \return a FeatureMatcher object
+     */
     FeatureMatcher(ros::NodeHandle& nh, int min_hessian);
-    void imageSubscriberCallback(const jps_puzzle_piece::ImageWithContourPtr& msg);
+
+    /*
+     * \brief Callback function for image subscriber
+     *
+     * \param[in] msg A message of type ImageWithContour containing the image,
+     * the contour of the piece, and its centroid
+     */
+    void imageSubscriberCallback(
+        const jps_puzzle_piece::ImageWithContourPtr& msg);
 };
 
 FeatureMatcher::FeatureMatcher(ros::NodeHandle& nh, int min_hessian)
@@ -50,7 +96,6 @@ FeatureMatcher::FeatureMatcher(ros::NodeHandle& nh, int min_hessian)
   this->nh = nh;
   this->min_hessian = min_hessian;
   this->surf_detector = SURF::create(min_hessian);
-  this->img_transport = new image_transport::ImageTransport(this->nh);
   this->image_sub = this->nh.subscribe(
       "input_image",
       1,
@@ -105,8 +150,14 @@ FeatureMatcher::FeatureMatcher(ros::NodeHandle& nh, int min_hessian)
     ROS_INFO_STREAM("Loading template image " << img_template_name << "...");
     this->img_template = imread(img_template_name, CV_LOAD_IMAGE_COLOR);
     ROS_INFO("Extracting SURF features from template image...");
-    this->surf_detector->detectAndCompute(this->img_template, noArray(), this->kp_template, this->desc_template);
-    ROS_INFO_STREAM(this->kp_template.size() << " template elements extracted.");
+    this->surf_detector->detectAndCompute(
+        this->img_template,
+        noArray(),
+        this->kp_template,
+        this->desc_template);
+    ROS_INFO_STREAM(
+        this->kp_template.size()
+        << " template elements extracted.");
   }
   else
   {
@@ -114,13 +165,27 @@ FeatureMatcher::FeatureMatcher(ros::NodeHandle& nh, int min_hessian)
   }
 }
 
-void FeatureMatcher::imageSubscriberCallback(const jps_puzzle_piece::ImageWithContourPtr& msg)
+void FeatureMatcher::imageSubscriberCallback(
+    const jps_puzzle_piece::ImageWithContourPtr& msg)
 {
-  int i;
-  Mat img_input = cv_bridge::toCvCopy(msg->image, "bgr8")->image;
-  vector<KeyPoint> kp_piece;
-  Mat desc_piece;
   double dist_ratio_threshold = 0.7;
+  int i;
+  Mat desc_piece;
+  Mat h;
+  Mat img_input = cv_bridge::toCvCopy(msg->image, "bgr8")->image;
+  Mat img_matches;
+  Mat img_template_cp;
+  Ptr<DescriptorMatcher> matcher;
+  Scalar colour(int(0.741 * 256), int(0.447 * 256), int(0.000 * 256));
+  vector<DMatch> good_matches;
+  vector<KeyPoint> kp_piece;
+  vector<Point2f> pt_piece;
+  vector<Point2f> pt_template;
+  vector< vector<DMatch> > knn_matches;
+  vector< vector<Point> > pt_piece_contours;
+  vector< vector<Point> > pt_template_contours;
+  vector< vector<Point2f> > pt_piece_contours_f;
+  vector< vector<Point2f> > pt_template_contours_f;
 
   // Extract SURF features from input image of puzzle piece.
   ROS_INFO("Extracting SURF features from puzzle piece...");
@@ -129,26 +194,45 @@ void FeatureMatcher::imageSubscriberCallback(const jps_puzzle_piece::ImageWithCo
       noArray(),
       kp_piece,
       desc_piece);
-  ROS_INFO_STREAM(kp_piece.size() << " template elements extracted from puzzle piece.");
+  ROS_INFO_STREAM(
+      kp_piece.size()
+      << " template elements extracted from puzzle piece.");
 
   // Compare with SURF features extracted from template image.
   // https://docs.opencv.org/3.4/d5/d6f/tutorial_feature_flann_matcher.html
-  Ptr<DescriptorMatcher> matcher = DescriptorMatcher::create(DescriptorMatcher::FLANNBASED);
-  vector< vector<DMatch> > knn_matches;
-  vector<DMatch> good_matches;
+  matcher = DescriptorMatcher::create(DescriptorMatcher::FLANNBASED);
   ROS_INFO("Running k-NN matching...");
   matcher->knnMatch(desc_piece, this->desc_template, knn_matches, 2);
   ROS_INFO("Running comparison for good matches...");
-  vector<Point> pt_template;
-  vector<Point> pt_piece;
+  pt_piece_contours.push_back(vector<Point>());
+  pt_piece_contours_f.push_back(vector<Point2f>());
+
+  for(i = 0; i < msg->contour_px.size(); ++i)
+  {
+    pt_piece_contours[0].push_back(
+        Point(msg->contour_px[i].x, msg->contour_px[i].y));
+    pt_piece_contours_f[0].push_back(
+        Point2f(msg->contour_px[i].x, msg->contour_px[i].y));
+  }
+
+  ROS_INFO_STREAM(
+      pt_piece_contours_f[0].size()
+      << " contour points obtained for current piece.");
 
   for(i = 0; i < knn_matches.size(); ++i)
   {
-    if(knn_matches[i][0].distance < dist_ratio_threshold * knn_matches[i][1].distance)
+    if((knn_matches[i][0].distance
+          < dist_ratio_threshold * knn_matches[i][1].distance)
+        && (pointPolygonTest(
+            pt_piece_contours_f[0],
+            kp_piece[knn_matches[i][0].queryIdx].pt,
+            false)
+          > 0))
     {
       good_matches.push_back(knn_matches[i][0]);
       pt_piece.push_back(kp_piece[knn_matches[i][0].queryIdx].pt);
-      pt_template.push_back(this->kp_template[knn_matches[i][0].trainIdx].pt);
+      pt_template.push_back(
+          this->kp_template[knn_matches[i][0].trainIdx].pt);
     }
     else
     {
@@ -156,14 +240,47 @@ void FeatureMatcher::imageSubscriberCallback(const jps_puzzle_piece::ImageWithCo
     }
   }
 
-  ROS_INFO_STREAM(good_matches.size() << " good matches found.");
+  ROS_INFO_STREAM(good_matches.size() << " good SURF matches found.");
+  ROS_INFO("Drawing piece centroid...");
+  circle(
+       img_input,
+       Point2f(msg->centroid_px.x, msg->centroid_px.y),
+       16,
+       colour,
+       -1,
+       8,
+       0);
+  ROS_INFO("Drawing piece contour...");
+  // Draw the piece contour with a line thickness of 2.
+  drawContours(img_input, pt_piece_contours, 0, colour, 2);
 
-  ROS_INFO("Drawing matches...");
-  Mat img_matches;
+  ROS_INFO(
+      "Computing homographic transformation from piece to template...");
+  h = findHomography(pt_piece, pt_template, CV_RANSAC);
+  pt_template_contours_f.push_back(vector<Point2f>(
+        pt_piece_contours_f[0].size()));
+
+  ROS_INFO("Transforming piece contour into image template...");
+  perspectiveTransform(pt_piece_contours_f[0], pt_template_contours_f[0], h);
+  ROS_INFO("Transforming piece contour into image template...");
+  img_template_cp = this->img_template.clone();
+  pt_template_contours.push_back(vector<Point>());
+
+  for(i = 0; i < pt_template_contours_f[0].size(); ++i)
+  {
+    pt_template_contours[0].push_back(
+        Point(
+          (int) pt_template_contours_f[0][i].x,
+          (int) pt_template_contours_f[0][i].y));
+  }
+
+  drawContours(img_template_cp, pt_template_contours, 0, colour, 2);
+
+  ROS_INFO("Drawing matched SURF features on piece and template...");
   drawMatches(
       img_input,
       kp_piece,
-      this->img_template,
+      img_template_cp,
       this->kp_template,
       good_matches,
       img_matches,
@@ -171,10 +288,9 @@ void FeatureMatcher::imageSubscriberCallback(const jps_puzzle_piece::ImageWithCo
       Scalar::all(-1),
       vector<char>(),
       DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
+
   imshow("Good matches", img_matches);
   waitKey(0);
-
-  Mat h = findHomography(pt_piece, pt_template, CV_RANSAC);
 }
 
 int main(int argc, char **argv)
